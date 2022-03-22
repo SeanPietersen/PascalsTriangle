e33d78a39e30d5e2e6b99f16c159c50261322295
Sean Pietersen <seanpietersen7@gmail.com>
2022-03-22 09:54:03 +0200

    Feat: Added API

---
+++ PascalsTriangle/PascalsTriangle.API/Controllers/PascalsTriangleController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PascalsTriangle.App;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PascalsTriangle.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PascalsTriangleController : ControllerBase
+    {
+        [HttpGet("pascal/{depth}")]
+        public ActionResult GetPascalsTriangle(long depth)
+        {
+
+            IDisplayPascalsTriangle pascalTriangle = new DisplayPascalsTriangle();
+
+            var result = pascalTriangle.Pascal(depth);
+
+            return Ok(result);
+
+        }
+    }
+}
